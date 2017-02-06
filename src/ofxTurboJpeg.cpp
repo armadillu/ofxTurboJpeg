a0b432fe4e2d5be3fc8490127fd130ff2e11e78f
Oriol Ferrer Mesia <oriol@uri.cat>
2017-02-06 11:45:15 -0500

    api cleanup to match of, header cleanup, deprecate ofImage based api

--- src/ofxTurboJpeg.cpp
+++ src/ofxTurboJpeg.cpp
@@ -1,30 +1,24 @@
 #include "ofxTurboJpeg.h"
 #include <sys/stat.h>
 
-ofxTurboJpeg::ofxTurboJpeg()
-{
+ofxTurboJpeg::ofxTurboJpeg(){
 
 	handleCompress = tjInitCompress();
-	if (handleCompress == NULL)
-	{
-		printf("Error in tjInitCompress():\n%s\n", tjGetErrorStr());
+	if (handleCompress == NULL){
+		ofLogError("ofxTurboJpeg") << "Error in tjInitCompress():" << tjGetErrorStr();
 	}
 
 	handleDecompress = tjInitDecompress();
-	if (handleDecompress == NULL)
-	{
-		printf("Error in tjInitDeCompress():\n%s\n", tjGetErrorStr());
+	if (handleDecompress == NULL){
+		ofLogError("ofxTurboJpeg") << "Error in tjInitDeCompress():" << tjGetErrorStr();
 	}
 }
 
-ofxTurboJpeg::~ofxTurboJpeg()
-{
-	if (handleCompress)
-	{
+ofxTurboJpeg::~ofxTurboJpeg(){
+	if (handleCompress){
 		tjDestroy(handleCompress);
 	}
-	if (handleDecompress)
-	{
+	if (handleDecompress){
 		tjDestroy(handleDecompress);
 	}
 	
@@ -32,26 +26,65 @@ ofxTurboJpeg::~ofxTurboJpeg()
 	handleDecompress = NULL;
 }
 
-//rgb only for now...
-void ofxTurboJpeg::save( ofImage * img, string fileName, int jpegQuality ){
+
+bool ofxTurboJpeg::load(ofPixels &pix, const string & path){
+	ofBuffer buf = ofBufferFromFile(path, true);
+	if (buf.size() == 0) return false;
+	return load(pix, buf);
+}
+
+
+bool ofxTurboJpeg::load(ofPixels &pix, const ofBuffer& buf){
+	int w, h;
+	int subsamp;
+	int ok = tjDecompressHeader2(handleDecompress, (unsigned char*)buf.getData(), buf.size(), &w, &h, &subsamp);
+
+	if (ok != 0){
+		ofLogError("ofxTurboJpeg") << "Error in tjDecompressHeader2():" << tjGetErrorStr() ;
+		return false;
+	}
+
+	pix.allocate(w, h, 3);
+	tjDecompress(handleDecompress, (unsigned char*)buf.getData(), buf.size(), pix.getData(), w, 0, h, 3, 0);
+	return true;
+}
+
+
+bool ofxTurboJpeg::load(ofImage &img, const string & path){
+	ofPixels pix;
+	if (!load(pix, path)) return false;
+	img.setFromPixels(pix);
+	return true;
+}
+
+
+bool ofxTurboJpeg::load(ofImage &img, const ofBuffer& buf){
+	ofPixels pix;
+	if (!load(pix, buf)) return false;
+	img.setFromPixels(pix);
+	return true;
+}
+
+
+void ofxTurboJpeg::save(ofImage * img, const string & path, int jpegQuality ){
     
 	if (img == NULL) return;
     
 	int pitch = 0, flags = 0, jpegsubsamp = 0;
 	unsigned long size = 0;
 	int bpp = 3;	//rgb only for now...
-unsigned char * output = (unsigned char*) malloc ( sizeof(char) * img->getWidth() * img->getHeight() * bpp );
+	unsigned char * output = (unsigned char*) malloc ( sizeof(char) * img->getWidth() * img->getHeight() * bpp );
 	tjCompress(handleCompress, img->getPixels().getData(), img->getWidth(), pitch, img->getHeight(), bpp, output, &size, jpegsubsamp, jpegQuality, flags);
     
-	string filePath = ofToDataPath( fileName, false);
+	string filePath = ofToDataPath( path, false);
 	FILE * file = fopen( filePath.c_str(), "wb");
 	fwrite ( output , 1 , size , file );
 	fclose( file);
 	free (output);
 }
 
-void ofxTurboJpeg::save(ofBuffer &buf, const ofPixels& pix, int jpegQuality)
-{
+
+void ofxTurboJpeg::save(ofBuffer &buf, const ofPixels& pix, int jpegQuality){
 	int pitch = 0, flags = 0, jpegsubsamp = 0;
 	unsigned long size = 0;
 	
@@ -110,37 +143,14 @@ void ofxTurboJpeg::save(ofBuffer &buf, const ofPixels& pix, int jpegQuality)
 	} 
 }
 
-void ofxTurboJpeg::save(string path, const ofPixels& pix, int jpegQuality)
-{
+void ofxTurboJpeg::save(const ofPixels& pix, const string & path, int jpegQuality){
 	ofBuffer buf;
 	save(buf, pix, jpegQuality);
 	ofBufferToFile(path, buf, true);
 }
 
-bool ofxTurboJpeg::load(string path, ofPixels &pix)
-{
-	ofBuffer buf = ofBufferFromFile(path, true);
-	if (buf.size() == 0) return false;
-	return load(buf, pix);
-}
-
-bool ofxTurboJpeg::load(const ofBuffer& buf, ofPixels &pix)
-{
-	int w, h;
-	int subsamp;
-	int ok = tjDecompressHeader2(handleDecompress, (unsigned char*)buf.getData(), buf.size(), &w, &h, &subsamp);
-	
-	if (ok != 0)
-	{
-		printf("Error in tjDecompressHeader2():\n%s\n", tjGetErrorStr());
-		return false;
-	}
-	
-	pix.allocate(w, h, 3);
-	
-	tjDecompress(handleDecompress, (unsigned char*)buf.getData(), buf.size(), pix.getData(), w, 0, h, 3, 0);
-	
-	return true;
+void ofxTurboJpeg::save(const ofImage& img, const string & path, int jpegQuality){
+	save(img.getPixels(), path, jpegQuality);
 }
 
 
--- src/ofxTurboJpeg.h
+++ src/ofxTurboJpeg.h
@@ -11,75 +11,56 @@ public:
 
 	ofxTurboJpeg();
 	~ofxTurboJpeg();
-    
-    template <class T>
-    bool compress( T & img, int jpegQuality, ofBuffer & buffer){
-        
-        if (img.getWidth() == 0) return NULL;
-        
-        int pitch = 0, flags = 0, jpegsubsamp = 0;
-        buffer.clear();
-        
-        unsigned int bpp = img.getPixels().getNumChannels();
-        unsigned int bytesPerLine = img.getPixels().getBytesPerPixel() *  img.getWidth();
-        unsigned char* data = NULL;
-        
-        unsigned long size = 0;
-        
-        if ( bpp == 1 ){
-            jpegsubsamp = TJSAMP_GRAY;
-        }
-        
-        int ok = tjCompress2(handleCompress, img.getPixels().getData(), img.getWidth(), bytesPerLine, img.getHeight(), getTJPixelFormat(img.getPixels().getImageType()), &data, &size, jpegsubsamp, jpegQuality, flags);
-        
-        if (ok != 0)
-        {
-            ofLogError("Error in tjCompress():")<<tjGetErrorStr();
-            tjFree(data);
 
-            return false;
-        } else {
-            buffer.set((char*)(data), size);
-            tjFree(data);
-            return true;
-        }
-    }
-    
-    void save( ofImage * img, string path, int jpegQuality );
-	void save(string path, const ofPixels& img, int jpegQuality = 90);
+	bool load(ofPixels & pix, const string & path);
+	bool load(ofPixels & pix, const ofBuffer & buf);
 
-	void save(string path, ofImage& img, int jpegQuality = 90)
-	{
-		save(path, img.getPixels(), jpegQuality);
+	void save(const ofPixels & img, const string & path, int jpegQuality = 90);
+	void save(ofBuffer & buf, const ofPixels & pix, int jpegQuality = 90);
+
+	OF_DEPRECATED_MSG("Use ofPixels api instead.", bool load(ofImage & img, const string & path));
+	OF_DEPRECATED_MSG("Use ofPixels api instead.", bool load(ofImage & img, const ofBuffer & buf));
+
+	OF_DEPRECATED_MSG("Use ofPixels api instead.", void save(ofImage * img, const string & path, int jpegQuality = 90));
+	OF_DEPRECATED_MSG("Use ofPixels api instead.", void save(const ofImage & img, const string & path, int jpegQuality = 90));
+
+	template <class T>
+	bool compress( T & img, int jpegQuality, ofBuffer & buffer){
+
+		if (img.getWidth() == 0) return NULL;
+
+		int pitch = 0, flags = 0, jpegsubsamp = 0;
+		buffer.clear();
+
+		unsigned int bpp = img.getPixels().getNumChannels();
+		unsigned int bytesPerLine = img.getPixels().getBytesPerPixel() *  img.getWidth();
+		unsigned char* data = NULL;
+
+		unsigned long size = 0;
+
+		if ( bpp == 1 ){
+			jpegsubsamp = TJSAMP_GRAY;
+		}
+
+		int ok = tjCompress2(handleCompress, img.getPixels().getData(), img.getWidth(), bytesPerLine, img.getHeight(), getTJPixelFormat(img.getPixels().getImageType()), &data, &size, jpegsubsamp, jpegQuality, flags);
+
+		if (ok != 0)
+		{
+			ofLogError("Error in tjCompress():")<<tjGetErrorStr();
+			tjFree(data);
+
+			return false;
+		} else {
+			buffer.set((char*)(data), size);
+			tjFree(data);
+			return true;
+		}
 	}
-	
-	bool load(string path, ofPixels &pix);
-	bool load(const ofBuffer& buf, ofPixels &pix);
 
-	bool load(string path, ofImage &img)
-	{
-		ofPixels pix;
-		if (!load(path, pix)) return false;
-
-		img.setFromPixels(pix);
-		return true;
-	}
-	
-	bool load(const ofBuffer& buf, ofImage &img)
-	{
-		ofPixels pix;
-		if (!load(buf, pix)) return false;
-
-		img.setFromPixels(pix);
-		return true;
-	}
-	
-	
 private:
 
 	tjhandle handleCompress;
 	tjhandle handleDecompress;
 
-    void save(ofBuffer &buf, const ofPixels& img, int jpegQuality = 90);
     int getTJPixelFormat( ofImageType fmt );
 };
