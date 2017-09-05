cc8a43f1c7f55e67792337b53c57660b489dc337
kidapu <tatsuya.kida@i-studio.co.jp>
2017-09-05 16:19:43 +0900

    Fix it to work with Ubuntu 16.04

--- addon_config.mk
+++ addon_config.mk
@@ -1,16 +1,19 @@
 meta:
 	ADDON_NAME = ofxTurboJpeg
 	ADDON_DESCRIPTION = OF wrapper for turbo-jpeg
-	ADDON_AUTHOR = Oriol Ferrer Mesià, Satoru Higa, Brett Renfer
+	ADDON_AUTHOR = Oriol Ferrer Mesi, Satoru Higa, Brett Renfer
 	ADDON_TAGS = "jpeg" "turboJPEG" "binary" "image"
 	ADDON_URL = https://github.com/robotconscience/ofxTurboJpeg
 
-common:
+osx:
 	ADDON_INCLUDES = src/ 
 	ADDON_INCLUDES += libs/turbo-jpeg/include
 
 	ADDON_SOURCES = src/ofxTurboJpeg.h
 	ADDON_SOURCES += src/ofxTurboJpeg.cpp
+
+linux64:
+	ADDON_LDFLAGS = -lturbojpeg
 
 vs:
 	# These should get automatically set up by the projectGenerator. If you're adding
